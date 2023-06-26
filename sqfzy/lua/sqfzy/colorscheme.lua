b07eb4aad8c1fae6c6afddbf9f70901f05049696
Jiang tao <101503109+sqfzy@users.noreply.github.com>
2023-06-26 21:35:25 +0800

    Delete sqfzy/lua/sqfzy directory

--- sqfzy/lua/sqfzy/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local hr = tonumber(os.date("%H", os.time()))
-lvim.colorscheme = "tokyonight-moon"
--- vim.cmd.colorscheme("tokyonight-day")
--- if hr > 7 and hr < 18 then
--- 	-- vim.cmd.colorscheme("github_light")
--- else
--- 	vim.cmd.colorscheme("tokyonight-moon")
--- 	-- vim.cmd.colorscheme("catppuccin")
--- end
